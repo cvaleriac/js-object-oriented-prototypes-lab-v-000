ee4afc2374456710c6d99acafec91436eb5c8e75
valeria campilongo <cvaleriac@hotmail.it>
2019-06-07 18:26:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ function BoardMember(name, homeState, training){
   this.training = training;
 };
 
-User.prototype.veto = function() {
+BoardMember.prototype.veto = function() {
     retun "No, I must disagree");
 };
 
-User.prototype.approve = function() {
+BoardMember.prototype.approve = function() {
     retun "You can do that");
 };
 
-User.prototype.doCharity = function() {
+BoardMember.prototype.doCharity = function() {
     retun "I like to help people");
 };
 
-User.prototype.doCharity = function() {
+BoardMember.prototype.doCharity = function() {
     retun "You will see great things from Scuber");
 };
 
-User.prototype.sayHi = function() {
+BoardMember.prototype.sayHi = function() {
     retun "Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.");
 };
 
