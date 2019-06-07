f93a5f8448b25c8d9b15af7fc16eb9241eab1810
valeria campilongo <cvaleriac@hotmail.it>
2019-06-07 18:27:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homeState, training){
     this.homeState = homeState
     this.training = training
 }
- 
+
 BoardMember.prototype.veto = function() {
     return 'No, I must disagree'
 }
@@ -23,4 +23,3 @@ BoardMember.prototype.releasePressStatement = function() {
 BoardMember.prototype.sayHi = function() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
-
