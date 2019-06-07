cab76fa125f1f0fa67d537333e9ece57716f040f
valeria campilongo <cvaleriac@hotmail.it>
2019-06-07 18:24:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+}
+
+User.prototype.veto = function() {
+    retun "No, I must disagree");
+};
+
+User.prototype.approve = function() {
+    retun "You can do that");
+};
+
+User.prototype.doCharity = function() {
+    retun "I like to help people");
+};
+
+User.prototype.doCharity = function() {
+    retun "You will see great things from Scuber");
+};
+
+User.prototype.sayHi = function() {
+    retun "Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.");
+};
+
+
 //   this.veto = function(){
 //     return 'No, I must disagree'
 //   }
